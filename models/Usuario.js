5ca8aeab742b55a06d1f532a58be23258ab0de4a
renanrosas <renan-rosas@hotmail.com>
2020-10-07 22:54:16 -0300

    user registration

---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const Usuario = new Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    admin: {
+        type: Number,
+        default: 0
+    },
+    senha: {
+        type: String,
+        require: true
+    }
+})
+
+mongoose.model("usuarios", Usuario)
